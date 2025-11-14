7d4f46203527397647f99e40e3a9d04d0b72e33b
Progga-Roy <proggaroy2000@gmail.com>
2025-11-14 22:50:01 +0600

    Logout the site

--- tests/loginToLogout.spec.js
+++ tests/loginToLogout.spec.js
@@ -4,8 +4,8 @@ import { test, expect } from '@playwright/test';
 test('A user logs in with valid credentials, adds one product to the cart, verifies the product name in the cart, and logs out',
  async ({ page }) => {
  await page.goto('https://www.saucedemo.com/');
- // Logs in with valid credentials
- await page.fill("#user-name","standard_user")
+ // Login with valid credentials
+ await page.fill("//input[@id='user-name']","standard_user")
  await page.fill("//input[@id='password']","secret_sauce");
  await page.click("//input[@id='login-button']");
  await page.waitForTimeout(1000);
@@ -15,9 +15,11 @@ test('A user logs in with valid credentials, adds one product to the cart, verif
  //Click cart icon to verify the product name
  await page.click("//a[@class='shopping_cart_link']")
  await expect(page.locator("//div[@class='inventory_item_name']")).toHaveText("Sauce Labs Bike Light");
- await page.waitForTimeout(2000);
-
-
+ await page.waitForTimeout(1000);
+ //Logout the site
+ await page.click("//button[@id='react-burger-menu-btn']");
+ await page.click("//a[@id='logout_sidebar_link']"); 
+ await page.waitForTimeout(1000);
 });
 
 
